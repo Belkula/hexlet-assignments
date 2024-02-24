9bec59e4f2f6d45a278a0bd1a40372ede9f08e62
@hexlet/cli <support@hexlet.io>
2024-02-24 22:02:10 +0300

    submit java-collections-ru/maps

--- java-collections-ru/maps/src/main/java/exercise/App.java
+++ java-collections-ru/maps/src/main/java/exercise/App.java
@@ -4,10 +4,12 @@ import java.util.HashMap;
 import java.util.Map;
 
 // BEGIN
-
 public class App {
     public static Map<String, Integer> getWordCount(String sentence) {
         Map<String, Integer> wordCount = new HashMap<>();
+        if (sentence.isEmpty()) {
+            return wordCount;
+        }
         String[] words = sentence.split("\\s+");
         for (String word : words) {
             wordCount.put(word, wordCount.getOrDefault(word, 0) + 1);
@@ -16,6 +18,9 @@ public class App {
     }
 
     public static String toString(Map<String, Integer> wordCount) {
+        if (wordCount.isEmpty()) {
+            return "{}";
+        }
         StringBuilder result = new StringBuilder("{\n");
         for (Map.Entry<String, Integer> entry : wordCount.entrySet()) {
             result.append("  ").append(entry.getKey()).append(": ").append(entry.getValue()).append("\n");
