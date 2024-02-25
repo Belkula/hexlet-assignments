7ff1dd034145aba26701f26e85436d661407ae4e
@hexlet/cli <support@hexlet.io>
2024-02-25 21:34:07 +0300

    submit java-collections-ru/lambdas

---
+++ java-collections-ru/lambdas/src/test/java/exercise/AppTest.java
@@ -0,0 +1,32 @@
+package exercise;
+
+import org.junit.jupiter.api.Test;
+import static org.assertj.core.api.Assertions.assertThat;
+
+// BEGIN
+class AppTest {
+
+    @Test
+    void testEnlargeArrayImage() {
+        String[][] image = {
+                {"*", "*", "*", "*"},
+                {"*", " ", " ", "*"},
+                {"*", " ", " ", "*"},
+                {"*", "*", "*", "*"},
+        };
+
+        String[][] expected = {
+                {"*", "*", "*", "*", "*", "*", "*", "*"},
+                {"*", "*", "*", "*", "*", "*", "*", "*"},
+                {"*", "*", " ", " ", " ", " ", "*", "*"},
+                {"*", "*", " ", " ", " ", " ", "*", "*"},
+                {"*", "*", " ", " ", " ", " ", "*", "*"},
+                {"*", "*", " ", " ", " ", " ", "*", "*"},
+                {"*", "*", "*", "*", "*", "*", "*", "*"},
+                {"*", "*", "*", "*", "*", "*", "*", "*"},
+        };
+
+        assertThat(App.enlargeArrayImage(image)).isEqualTo(expected);
+    }
+}
+// END
