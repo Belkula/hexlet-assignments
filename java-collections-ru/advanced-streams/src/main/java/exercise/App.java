30fe3202a9509a2f1c79eaeebde7a959fd037b70
@hexlet/cli <support@hexlet.io>
2024-02-26 20:00:14 +0300

    submit java-collections-ru/advanced-streams

--- java-collections-ru/advanced-streams/src/main/java/exercise/App.java
+++ java-collections-ru/advanced-streams/src/main/java/exercise/App.java
@@ -8,20 +8,24 @@ public class App {
     public static String getForwardedVariables(String content) {
         return Arrays.stream(content.split("\n"))
                 .filter(line -> line.contains("environment"))
-                .flatMap(line -> Arrays.stream(line.split("\"")))
-                .skip(1)
-                .findFirst()
-                .map(variables -> Arrays.stream(variables.split(","))
-                        .filter(variable -> variable.startsWith("X_FORWARDED_"))
-                        .map(variable -> {
-                            String[] keyValue = variable.split("=");
-                            if (keyValue.length == 2) {
-                                return keyValue[0].substring("X_FORWARDED_".length()) + "=" + keyValue[1];
-                            }
-                            return "";
-                        })
-                        .collect(Collectors.joining(",")))
-                .orElse("");
+                .flatMap(line -> {
+                    String[] parts = line.split("\"");
+                    if (parts.length > 1) {
+                        String[] variables = parts[1].split(",");
+                        return Arrays.stream(variables)
+                                .filter(variable -> variable.startsWith("X_FORWARDED_"))
+                                .map(variable -> {
+                                    String[] keyValue = variable.split("=");
+                                    if (keyValue.length == 2) {
+                                        return keyValue[0].substring("X_FORWARDED_".length()) + "=" + keyValue[1];
+                                    }
+                                    return "";
+                                });
+                    }
+                    return Arrays.stream(new String[]{});
+                })
+                .collect(Collectors.joining(","));
     }
 }
+In this revised ve
 //END
